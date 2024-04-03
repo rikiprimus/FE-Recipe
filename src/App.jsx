eb8b7af37335b9b8fdcb6340d0dc470a3ce96bc7
rikiprimus <rikiprimus33@gmail.com>
2024-04-03 14:38:19 +0700

    fix bug route

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,113 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
-import { Landing, Register, Login, ForgotPassowrd, ProfileEdit, PasswordEdit, Recipes, Bookmark, Create, Edit, Detail, Search } from './pages';
-import Auth from './components/Auth';
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import "./App.css";
+import {
+  Landing,
+  Register,
+  Login,
+  ForgotPassowrd,
+  ProfileEdit,
+  PasswordEdit,
+  Recipes,
+  Bookmark,
+  Create,
+  Edit,
+  Detail,
+  Search,
+} from "./pages";
+import Auth from "./components/Auth";
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Landing />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/forgotpassword" element={<ForgotPassowrd />} />
         <Route path="/recipes/search" element={<Search />} />
-        <Route path="" element={<Auth/>}>
-          <Route path="/profile" element={<ProfileEdit />} />
-          <Route path="/profile/edit" element={<ProfileEdit />} />
-          <Route path="/profile/changepassword" element={<PasswordEdit />} />
-          <Route path="/profile/recipes" element={<Recipes />} />
-          <Route path="/profile/bookmark" element={<Bookmark />} />
-          <Route path="/recipes/detail" element={<Detail />} />
-          <Route path="/recipes/detail/:id" element={<Detail />} />
-          <Route path="/recipes/add" element={<Create />} />
-          <Route path="/recipes/edit" element={<Edit />} />
-          <Route path="/recipes/edit/:id" element={<Edit />} />
-        </Route>
+        <Route
+          path="/profile"
+          element={
+            <Auth>
+              <ProfileEdit />
+            </Auth>
+          }
+        />
+        <Route
+          path="/profile/edit"
+          element={
+            <Auth>
+              <ProfileEdit />
+            </Auth>
+          }
+        />
+        <Route
+          path="/profile/changepassword"
+          element={
+            <Auth>
+              <PasswordEdit />
+            </Auth>
+          }
+        />
+        <Route
+          path="/profile/recipes"
+          element={
+            <Auth>
+              <Recipes />
+            </Auth>
+          }
+        />
+        <Route
+          path="/profile/bookmark"
+          element={
+            <Auth>
+              <Bookmark />
+            </Auth>
+          }
+        />
+        <Route
+          path="/recipes/detail"
+          element={
+            <Auth>
+              <Detail />
+            </Auth>
+          }
+        />
+        <Route
+          path="/recipes/detail/:id"
+          element={
+            <Auth>
+              <Detail />
+            </Auth>
+          }
+        />
+        <Route
+          path="/recipes/add"
+          element={
+            <Auth>
+              <Create />
+            </Auth>
+          }
+        />
+        <Route
+          path="/recipes/edit"
+          element={
+            <Auth>
+              <Edit />
+            </Auth>
+          }
+        />
+        <Route
+          path="/recipes/edit/:id"
+          element={
+            <Auth>
+              <Edit />
+            </Auth>
+          }
+        />
+        <Route path="/" element={<Outlet />} />
       </Routes>
-  </BrowserRouter>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
